e2cf545990875bfa69748518abf5f167452b344b
Administrator <Administrator@DELL>
2020-08-10 13:14:19 +0530

    Project guide file updated.

--- HibernateConnectionPoolc3p0Demo/src/main/java/com/javaLive/databaseUtil/HibernateUtil.java
+++ HibernateConnectionPoolc3p0Demo/src/main/java/com/javaLive/databaseUtil/HibernateUtil.java
@@ -49,21 +49,9 @@ public class HibernateUtil {
 		}
 		return sessionFactory;
 	}
-
 	public static void shutdown() {
 		if (registry != null) {
 			StandardServiceRegistryBuilder.destroy(registry);
 		}
 	}
-
-	/*
-	 * private static final SessionFactory sessionFactory1;
-	 * 
-	 * static { try { sessionFactory1 = new
-	 * AnnotationConfiguration().configure().buildSessionFactory(); } catch
-	 * (Throwable ex) { System.err.println("Initial SessionFactory creation failed."
-	 * + ex); throw new ExceptionInInitializerError(ex); } }
-	 * 
-	 * public static SessionFactory getSessionFactory1() { return sessionFactory1; }
-	 */
 }
